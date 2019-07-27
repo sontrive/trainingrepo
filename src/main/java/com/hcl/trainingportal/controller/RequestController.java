71b3375f3c02db986ca36e877e5bf5a77fdbfb1b
Rajat <bigdatarajatpancholi@gmail.com>
2019-07-27 15:24:05 +0530

    approval request

--- src/main/java/com/hcl/trainingportal/controller/RequestController.java
+++ src/main/java/com/hcl/trainingportal/controller/RequestController.java
@@ -1,13 +1,14 @@
 package com.hcl.trainingportal.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.hcl.trainingportal.dto.RequestApprovalDTO;
-import com.hcl.trainingportal.dto.TraineeDTO;
+import com.hcl.trainingportal.dto.ResponseDTO;
 import com.hcl.trainingportal.exception.ApplicationException;
 import com.hcl.trainingportal.service.RequestService;
 
@@ -24,8 +25,10 @@ private static final String ERROR_MSG = "Mandetory Element missing : ";
 		if(requestApprovalDTO.getTrainerId() == null || requestApprovalDTO.getRequestId() == 0) {
 			throw new ApplicationException("Trainer id is not valid");
 		}
-		
-		//return new ResponseEntity<>(requestService.registerUser(traineeDTO), HttpStatus.OK);
-		return null;
+		requestService.approve(requestApprovalDTO);
+		ResponseDTO respDTO = new ResponseDTO();
+		respDTO.setMessage("Your request is "+requestApprovalDTO.getStatus());
+		respDTO.setData(requestApprovalDTO);
+		return new ResponseEntity<>(respDTO, HttpStatus.OK) ;
 	}
 }
